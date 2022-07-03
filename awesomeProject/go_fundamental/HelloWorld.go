8e40345a825dee922ca0150df2f9b2ccd825a277
longtran <tranlong204@gmail.com>
2022-07-02 21:41:11 -0700

    First HelloWorld

--- awesomeProject/go_fundamental/HelloWorld.go
+++ awesomeProject/go_fundamental/HelloWorld.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fmt.Println("This is the most awesome program in the world")
+
+	foo()
+
+	for i := 0; i < 100; i++ {
+		if i%2 == 0 {
+			fmt.Print(i)
+		}
+	}
+
+}
+
+func foo() {
+	fmt.Println("This is foo")
 }
