eddfdab14b89f9fb436de46221aecfd93c6ba028
longtran <tranlong204@gmail.com>
2022-07-05 23:00:27 -0700

    new code

---
+++ awesomeProject/leetcode_DP1/Day2_ClimbingStairs.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(climbStairs(4))
+}
+
+func climbStairs(n int) int {
+	a := 1
+	b := 1
+	c := 0
+	if n == 1 {
+		return 1
+	} else {
+		for i := 2; i <= n; i++ {
+			c = a + b
+			a = b
+			b = c
+		}
+	}
+	return b
+}
